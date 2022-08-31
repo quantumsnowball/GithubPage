8620881e77f6690f6793d3684d2d8dfd66ebe6cd
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-31 20:53:52 +0800

    feat: simple appbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,23 @@ import '../styles/App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>
-          Hello World!
-          SASS flexbox
-        </h1>
+    <div className="app-ctn">
+      <div className="appbar-ctn">
+        <h3>Quantum Snowball</h3>
+        <div style={{ flexGrow: 1 }} />
+        <div>
+          <ul>
+            <li><a href="/apps">Apps</a></li>
+            <li><a href="/web-dev">Web Dev</a></li>
+            <li><a href="/apple-daily">Apple Daily</a></li>
+            <li><a href="/research">Research</a></li>
+            <li><a href="/data-science">Data Science</a></li>
+            <li><a href="/dotfiles">dotfiles</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="main-ctn">
+        <h3>Main Area</h3>
       </div>
     </div>
   )
